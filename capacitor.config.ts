eb32d064ab1024f9d8cffe1178abc97aa029cd6b
Mohammad Fareed <fareed543@gmail.com>
2025-10-22 11:10:17 +0530

    Feature : Android App
    1. Install capacitor
    2. generate apk in assigned
    3. Generate keystore
    4. Generate signed apk

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.example.app',
+  appName: 'salah-time-board',
+  webDir: 'dist/salah-time-board/browser', // <-- include /browser
+};
+
+export default config;
