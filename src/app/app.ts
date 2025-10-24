2c82941eb60055a946b39eef37ad63da06368de6
Mohammad Fareed <fareed543@gmail.com>
2025-10-24 07:11:34 +0530

    HTML5up Theme setup

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,6 @@
-import { Component, signal } from '@angular/core';
+import { AfterViewInit, Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LayoutService } from './layout';
 
 @Component({
   selector: 'app-root',
@@ -7,6 +8,21 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
-export class App {
+export class App implements AfterViewInit {
   protected readonly title = signal('salah-time-board');
+
+  constructor(private layoutService: LayoutService) {}
+
+
+  ngAfterViewInit() {
+    this.layoutService.setPreload(true);
+    this.layoutService.setResizingHandlers();
+    this.layoutService.setupMenuToggles();
+  }
+
+  toggleSidebar(event: Event) {
+    event.preventDefault();
+    event.stopPropagation();
+    this.layoutService.toggleSidebar();
+  }
 }
